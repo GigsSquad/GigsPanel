2c8dbc79afa25d4fadb058a04c4850458ed7f8d1
evelan <evelan3094@gmail.com>
2015-04-14 02:40:37 +0200

    zapomniane pliki :v

---
+++ GigsPanel/gigs-worker-backend/src/main/java/it/coderunner/gigs/worker/backend/App.java
@@ -0,0 +1,13 @@
+package it.coderunner.gigs.worker.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
