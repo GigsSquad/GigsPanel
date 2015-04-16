7394fd02993e7edd05e643f13340345a0108706d
Rafal <rkpieniazek@gmail.com>
2015-04-16 12:34:12 +0200

    zmiany w model.user

--- GigsPanel/gigs-model/src/main/java/it/coderunner/gigs/model/user/User.java
+++ GigsPanel/gigs-model/src/main/java/it/coderunner/gigs/model/user/User.java
@@ -2,13 +2,23 @@ package it.coderunner.gigs.model.user;
 
 import it.coderunner.gigs.model.BaseEntity;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -45,5 +55,67 @@ public class User extends BaseEntity<Long>{
 	
 	@Getter @Setter
 	private Date registered;
-
+	
+	@Enumerated(EnumType.STRING)
+	@Getter @Setter
+	private UserStatus status;
+	
+	@Getter @Setter
+	private UserLocale locale = UserLocale.pl;
+	
+	/**
+	 * Rola użytkownika
+	 */
+	@ElementCollection(targetClass = UserRole.class, fetch=FetchType.LAZY)
+	@CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), 
+		uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "role" }))
+	@Column(name = "role", columnDefinition="TEXT") 
+	@Enumerated(EnumType.STRING)
+	@Getter @Setter
+	private Set<UserRole> roles = new HashSet<>();
+	
+	/**
+	 * Dodaje zbiór ról do zbioru ról posiadanych przez użytkownika
+	 * @param roles
+	 * @return zwraca true, jeżeli zbiór ról posiadanych przez użytkownika zmienił się po operacji dodawania.
+	 */
+	public boolean add(UserRole... roles) {
+		return this.roles.addAll(Arrays.asList(roles));
+	}
+	
+	/**
+	 * Usuwa zbiór ról ze zbioru ról posiadanych przez użytkownika
+	 * @param roles
+	 * @return zwraca true, jeżeli zbiór ról posiadanych przez użytkownika zmienił się po operacji usuwania.
+	 */
+	public boolean remove(UserRole...roles ) {
+		return this.roles.removeAll(Arrays.asList(roles));
+	}
+	
+	/**
+	 * Sprawdza, czy użytkownik posiada wszystkie role zadeklarowane jako parametry metod. 
+	 * Jeżeli nie posiada choćby jednej, metoda zwraca false.
+	 * @param roles
+	 * @return
+	 */
+	public boolean hasRoles(UserRole...roles ){
+		for(UserRole role : roles){
+			if(!this.getRoles().contains(role)){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Sprawdza, czy użytkownik posiada dany status
+	 * @param status
+	 * @return Jeżeli użytkownik
+	 */
+	public boolean hasStatus(UserStatus status){
+		if(status==null || this.status==null){
+			return false;
+		}
+		return this.status.equals(status);
+	}
 }
---
+++ GigsPanel/gigs-model/src/main/java/it/coderunner/gigs/model/user/UserRole.java
@@ -0,0 +1,11 @@
+package it.coderunner.gigs.model.user;
+
+public enum UserRole {
+	
+	/**
+	 * Role związane są z plikiem context-security
+	 */
+	ROLE_ADMIN,
+	
+	ROLE_USER;
+}
