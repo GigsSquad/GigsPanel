28670398036b911f6fbca3106f169295040dff57
evelan <evelan3094@gmail.com>
2015-05-04 22:02:50 +0200

    Dodalem I przed GigREpository i chuj

--- GigsPanel/gigs-repository/src/main/java/it/coderunner/gigs/repository/gigs/IGigRepository.java
+++ GigsPanel/gigs-repository/src/main/java/it/coderunner/gigs/repository/gigs/IGigRepository.java
@@ -3,7 +3,7 @@ package it.coderunner.gigs.repository.gigs;
 import it.coderunner.gigs.model.gig.Gig;
 import it.coderunner.gigs.repository.IRepository;
 
-public interface GigRepository extends IRepository<Gig, Long>{
+public interface IGigRepository extends IRepository<Gig, Long>{
 	
 	Gigs findAll();
 
