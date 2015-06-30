6aec668b3d3b58a24acda5ef582e118aad7e2777
stiff-dev <sztyvny@10g.pl>
2015-06-30 14:09:16 +0200

    sessionPreferences leci

---
+++ GigsPanel/gigs-webapp/src/main/java/it/coderunner/gigs/webapp/controller/SessionPreferences.java
@@ -0,0 +1,40 @@
+package it.coderunner.gigs.webapp.controller;
+
+import it.coderunner.gigs.model.user.User;
+import it.coderunner.gigs.repository.users.Users;
+import it.coderunner.gigs.service.users.IUserService;
+import it.coderunner.gigs.webapp.utils.SecurityUtils;
+
+import java.io.Serializable;
+
+import lombok.Setter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class SessionPreferences implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private IUserService userService;
+	@Setter
+	private String displayName;
+	
+	public String getDisplayName(){
+		if(StringUtils.isBlank(displayName)){
+			User user = userService.uniqueObject(Users.findAll().withUsername(SecurityUtils.getLoggedUsername()));
+			displayName = user.getUsername();
+		}
+		return displayName;		
+	}
+
+}
