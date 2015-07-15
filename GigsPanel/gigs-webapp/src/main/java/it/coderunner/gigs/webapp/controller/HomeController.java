38e6d2acf81603e0c8f090506d249c21aeb19deb
stiff-dev <sztyvny@10g.pl>
2015-07-15 13:47:11 +0200

    dashboard po zalogowaniu

--- GigsPanel/gigs-webapp/src/main/java/it/coderunner/gigs/webapp/controller/HomeController.java
+++ GigsPanel/gigs-webapp/src/main/java/it/coderunner/gigs/webapp/controller/HomeController.java
@@ -1,11 +1,23 @@
 package it.coderunner.gigs.webapp.controller;
 
+import it.coderunner.gigs.repository.artists.Artists;
+import it.coderunner.gigs.repository.gigs.Gigs;
+import it.coderunner.gigs.repository.gigs.IGigRepository;
+import it.coderunner.gigs.repository.spots.Spots;
+import it.coderunner.gigs.repository.users.Users;
+import it.coderunner.gigs.service.artists.IArtistService;
+import it.coderunner.gigs.service.gigs.IGigService;
+import it.coderunner.gigs.service.spots.ISpotService;
+import it.coderunner.gigs.service.users.IUserService;
+
 import javax.servlet.http.HttpServletRequest;
 
 import lombok.extern.log4j.Log4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,12 +25,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Log4j
 @RequestMapping(value = { "/user" })
 public class HomeController extends LoggedUserController{
+	@Autowired
+	private IUserService userService;
 	
+	@Autowired
+	private ISpotService spotService;
+	
+	@Autowired
+	private IArtistService artistService;
+	
+	@Autowired
+	private IGigService gigService;
+	
+	
+	@ModelAttribute("userCounter")
+	public int userCounter() {
+		return userService.list(Users.findAll()).size();
+	}
+	
+	@ModelAttribute("spotCounter")
+	public int spotCounter(){
+		return spotService.list(Spots.findAll()).size();
+	}
+	
+	@ModelAttribute("artistCounter")
+	public int artistCounter(){
+		return artistService.list(Artists.findAll()).size();
+	}
+	
+	@ModelAttribute("gigCounter")
+	public int gigCounter(){
+		return gigService.list(Gigs.findAll()).size();
+	}
 
 	@RequestMapping(value={"", "/", "index", "/index/"}, method = RequestMethod.GET)
-
 	public String home(Model model, HttpServletRequest request) {
-		
 		log.info("Żyję!!!");
 		return "index";
 	}
